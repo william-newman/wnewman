61241d39df82ffe416e62e41283c0cf125999206
William Newman <38147848+william-newman@users.noreply.github.com>
2024-06-02 22:36:45 -0600

    Add missing import extension

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import * as THREE from "three";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
 import { CubeComponent } from './cube/cube.component';
 
 @Component({
